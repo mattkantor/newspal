077da61958a8919fe8ac49091aac4e3529dd2be5
Matthew Kantor <matthewkantor@Matthews-MacBook-Pro.local>
2018-05-25 13:58:22 -0400

    sidekiq ubuntu

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,6 +12,26 @@ set :bundle_flags, '--deployment --quiet'
 set :ssh_options, {:forward_agent => true}
 
 after 'deploy:publishing', 'deploy:restart'
+after 'deploy:starting', 'sidekiq:quiet'
+after 'deploy:reverted', 'sidekiq:restart'
+after 'deploy:published', 'sidekiq:restart'
+
+
+namespace :sidekiq do
+  task :quiet do
+    on roles(:app) do
+      puts capture("pgrep -f 'sidekiq' | xargs kill -TSTP")
+    end
+  end
+  task :restart do
+    on roles(:app) do
+      execute :sudo, :initctl, :restart, :workers
+    end
+  end
+end
+
+
+
 namespace :deploy do
   task :restart do
     #invoke 'unicorn:restart'
