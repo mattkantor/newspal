e42b83ce583e1164835081e68c8020327240eb4a
Matthew Kantor <matthewkantor@Matthews-MacBook-Pro.local>
2018-05-25 21:56:00 -0400

    leans

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,12 @@
 module ApplicationHelper
-  def show_leans(item)
-    if item.source.leans >3
+  def show_leans(leans)
+    if leans >3
       return "Very Conservative"
-    elif item.source.leans <-3
+    elif leans <-3
       return "Very Liberal"
-    elsif item.source.leans>1
+    elsif leans>1
       return "Conservative"
-    elsif item.source.leans < -1
+    elsif leans < -1
       return "Liberal"
     else
       return "Neutral"
