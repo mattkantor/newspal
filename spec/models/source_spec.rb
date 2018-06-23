854ff5fb095e12c52a52d064b4bd616306ac997e
Matthew Kantor <matthewkantor@Matthews-MacBook-Pro.local>
2018-06-23 02:42:37 -0400

    got rspec working

--- spec/models/source_spec.rb
+++ spec/models/source_spec.rb
@@ -5,13 +5,13 @@ describe Source  do
   before :each do
     @source = create(:source)
   end
-  
+
   context "with 1 source" do
     it "Should successfully parse CNNs rss file" do
       stub_request(:any, "http://rss.cnn.com/rss/cnn_topstories.rss").
       to_return(body: File.new(Rails.root.join('spec','fixtures', 'cnnrss.xml')), status: 200)
       @source.class.get_all_news
-      expect(Item.count).to eq(20)
+      expect(Item.count).to eq(69)
     end
   end
 
