0012438f579b72aebe73ebc2b03c01650ad90b7f
Matthew Kantor <matthewkantor@Matthews-MacBook-Pro.local>
2018-07-06 15:12:02 -0400

    fixed bugs in category

--- app/models/category.rb
+++ app/models/category.rb
@@ -31,26 +31,29 @@ class Category < ApplicationRecord
     new_cat = Category.where(name:name).first_or_create
     to_delete = []
 
-    batch_action_collection.find(ids).each do |cat|
+    Category.find(ids).each do |cat|
       new_alias_tags = new_cat.alias_tags << cat.name
       new_cat.alias_tags = new_alias_tags.uniq
-      if new_cat.save?(validate:false)
+      if new_cat.save(validate:false)
         cat.update_attributes({status:1})
         #merge all the related tables
-        counts_to_merge = cat.items_categories
+        counts_to_merge = cat.category_counts
         counts_to_merge.each do |count_obj|
           #get the target object and increment, otherwise create
-          target_obj = CategoryCount.where(name:name, date:count_obj.date).first_or_create
-          target_obj.count = target_onj.count + count_obj.count
+          target_obj = CategoryCount.where(category_id:cat.id, run_date:count_obj.run_date).first_or_create
+          target_obj.count = target_obj.count + count_obj.count
+
+
+
           #need to re-calculate sentiment
-          if target_obj.save?(validate:false)
+          if target_obj.save(validate:false)
             to_delete << count_obj
           end
         end
 
 
       end
-      to_delete.destroy_all
+      to_delete.delete(true)
   end
 
   end
@@ -72,24 +75,34 @@ class Category < ApplicationRecord
     end
   end
 
+  def build_sums(the_date)
+    qry = []
+    things = [self.name] + self.alias_tags
+    things.each do |aliaz|
+      qry <<  "(lower(title) ~ '#{ aliaz.downcase}' or lower(body) ~ '#{ aliaz.downcase}')" if !aliaz.blank?
+    end
+    compound = qry.join(" OR ")
+    items_qry = Item.where("date(published)=? and (#{compound})", the_date)
+    items = items_qry.all
+    items_count = items.count
+    if items_count > 0
+      items_sent = (items.reduce(0) {|sum,i| sum + i.sentiment})/items_count
+      puts "#{self.name} has #{items_count} for #{the_date}"
+      CategoryCount.find_or_initialize_by(category_id:self.id,run_date:the_date).update_attributes!(count:items_count,  avg_sent:items_sent)
+    else
+      cat_count = CategoryCount.where(category_id:self.id).where(run_date:the_date).first
+    end
+  end
+
+  def get_item_data_for_sums(the_date)
+
+  end
+
   def self.compute_daily_trends(the_date=Time.now.to_date-1)
 
       Category.open.each do |cat|
-        qry = []
-        things = [cat.name] + cat.alias_tags
-        things.each do |aliaz|
-          qry <<  "(lower(title) ~ '#{ aliaz.downcase}' or lower(body) ~ '#{ aliaz.downcase}')" if !aliaz.blank?
-        end
-        compound = qry.join(" OR ")
-        items_qry = Item.where("date(published)=? and (#{compound})", the_date)
-        items = items_qry.all
-        items_count = items.count
-        if items_count > 0
-          items_sent = (items.reduce(0) {|sum,i| sum + i.sentiment})/items_count
-          puts "#{cat.name} has #{items_count} for #{the_date}"
-          CategoryCount.find_or_initialize_by(category_id:cat.id,run_date:the_date).update_attributes!(count:items_count,  avg_sent:items_sent)
-        end
-
+        #cat.category_counts.where(run_date:the_date).destroy_all
+        cat.build_sums(the_date)
       end
 
 
